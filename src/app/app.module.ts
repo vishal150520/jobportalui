0b8036d9143bbdf7c3d3e48d05bbd925f902dd9b
abhashk.sinha@scadea.com <abhashk.sinha@scadea.com>
2024-01-17 00:15:06 +0530

    home, about,contact page route fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Component/header/header.component';
 import { FooterComponent } from './Component/footer/footer.component';
+import { LoginComponent } from './Component/login/login.component';
+import { SignupComponent } from './Component/signup/signup.component';
+import { AboutComponent } from './Component/about/about.component';
+import { ContactComponent } from './Component/contact/contact.component';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
