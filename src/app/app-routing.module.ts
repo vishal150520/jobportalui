8baa59ba2b3d581ecb967fcacbb4a3181544d556
abhashk.sinha@scadea.com <abhashk.sinha@scadea.com>
2024-01-17 12:06:40 +0530

    logIN , Signup Page Fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './Component/about/about.component';
 import { ContactComponent } from './Component/contact/contact.component';
-import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './Component/login/login.component';
+import { SignupComponent } from './Component/signup/signup.component';
 
 const routes: Routes = [
   { path: '', component:HomeComponent },
   { path: 'about', component:AboutComponent},
   { path: 'contact', component:ContactComponent },
+  { path: 'login', component:LoginComponent },
+  { path: 'signup', component:SignupComponent },
 ];
 
 @NgModule({
